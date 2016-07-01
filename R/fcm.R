25147b4722d5d4c9266a518359d8724ff63961ce
Kenneth Benoit <kbenoit@lse.ac.uk>
2016-07-01 17:19:04 +0100

    Tidy up cfm() functions Rename fcm to cfm

--- R/fcm.R
+++ R/fcm.R
@@ -1,40 +1,41 @@
-#' Virtual class "fcm" for a feature-co-occurrence matrix
+#' Virtual class "cfm" for a context-feature matrix
 #' 
-#' @description The fcm class of object is a special type of \link{dfm-class}.
+#' @description The cfm class of object is a special type of \link{dfm-class}.
 #'   
 #' @slot context the context definition
 #' @slot window the size of the window, if \code{context = "window"}
 #' @slot weights context weighting for distance from target feature, equal in length to \code{window}
-#' @seealso \link{fcm}
+#' @seealso \link{cfm}
 #' @export
 #' @import methods
 #' @docType class
-#' @name fcm-class
-setClass("fcm",
+#' @name cfm-class
+setClass("cfm",
          slots = c(context = "character", window = "integer", weights = "numeric"),
-#         prototype = list(Dimnames = list(contextFeatures = NULL, targetFeatures = NULL)),
+         # prototype = list(Dimnames = list(contexts = NULL, features = NULL)),
          contains = c("dfm", "dgCMatrix"))
 
 
-#' create a feature co-occurrence matrix
+#' create a context-feature co-occurrence matrix
 #' 
-#' Create a sparse feature co-occurrence matrix within a user-defined context. 
-#' The context can be defined as a document or a window within a collection of
-#' documents, with an optional vector of weights applied to the co-occurrence
-#' counts.
-#' @param x character vector, corpus, or tokenized texts from which to generate
+#' Create a sparse context-feature co-occurrence matrix, measuring
+#' co-occurrences of features within a user-defined context. The context can be
+#' defined as a document or a window within a collection of documents, with an
+#' optional vector of weights applied to the co-occurrence counts.
+#' @param x character vector, corpus, or tokenized texts from which to generate 
 #'   the context-feature co-occurrence matrix.
-#' @param ... additional arguments passed to \link{tokenize}, which can include
-#'   for instance \code{ngrams} and \code{concatenator} for tokenizing
+#' @param ... additional arguments passed to \link{tokenize}, which can include 
+#'   for instance \code{ngrams} and \code{concatenator} for tokenizing 
 #'   multi-token sequences
+#' @author Kenneth Benoit (R) and Kohei Watanabe (C++)
 #' @import Matrix
 #' @export
-#' @name fcm
-fcm <- function(x, ...) {
-    UseMethod("fcm")
+#' @name cfm
+cfm <- function(x, ...) {
+    UseMethod("cfm")
 }
 
-#' @rdname fcm
+#' @rdname cfm
 #' @param context the context in which to consider term co-occurrence: 
 #'   \code{"document"} for co-occurrence counts within document; \code{"window"}
 #'   for co-occurrence within a defined window of words, which requires a 
@@ -45,145 +46,151 @@ fcm <- function(x, ...) {
 #' @param weights a vector of weights applied to each distance from
 #'   \code{1:window}, strictly decreasing and of the same length as
 #'   \code{length(weights)}
-#' @param spanSentence if \code{FALSE}, then word windows will not span
+#' @param span_sentence if \code{FALSE}, then word windows will not span
 #'   sentences
 #' @param tri if \code{TRUE} return only upper triangle (including diagonal)
 #' @param verbose if \code{TRUE} print status messages to the console
 #' @examples
+#' # see 
+#' txt <- "A D C E A D F E B A C E D"
+#' cfm(txt, context = "window", window = 2)
+#' 
 #' (txts <- c(paste(letters[c(1, 1:3)], collapse = " "), 
 #'            paste(letters[c(1, 3, 5)], collapse = " "), 
 #'            paste(letters[c(5, 6, 7)], collapse = " ")))
 #' toks <- tokenize(toLower(txts), removePunct = TRUE)
-#' fcm(toks, context = "document")
+#' cfm(toks, context = "document")
 #' 
 #' txt <- c("The quick brown fox jumped over the lazy dog.",
 #'          "The dog jumped and ate the fox.")
-#' toks <- tokenize(toLower(txt), removePunct = TRUE)
-#' fcm(toks, context = "window", window = 3)
-#' fcm(toks, context = "window", window = 2)
+#' cfm(toks, context = "document")
+#' cfm(toks, context = "window", window = 3)
+#' cfm(toks, context = "window", window = 2)
 #' @import data.table
 #' @import Matrix
 #' @export
-fcm.tokenizedTexts <- function(x, context = c("document", "window"), window = 5L,
+cfm.tokenizedTexts <- function(x, context = c("document", "window"), window = 5L,
                                weights = rep(1, length(window)),
-                               spanSentence = TRUE, tri = TRUE, ...) {
+                               span_sentence = TRUE, tri = TRUE, ...) {
     context <- match.arg(context)
-    
     feature <- V1 <- NULL  # to avoid no visible binding errors in CHECK
-    
     # could add a warning if not roundly coerced to integer
     window <- as.integer(window)
     
-    if (!spanSentence) 
+    if (!span_sentence) 
         warning("spanSentence = FALSE not yet implemented")
     
     if (context == "document") {
         
-        x <- tf(dfm(x, toLower = FALSE, verbose = FALSE), "boolean")
-        # get co-occurrence counts
-        result <- t(x) %*% x
-        # remove 1 from the diagonal so that target features are not counted as their own context
-        Matrix::diag(result) <- ifelse(Matrix::diag(result) > 0, Matrix::diag(result) - 1, 0)
-        # return triangular result only if tri == TRUE
-        if (tri) 
-            result[lower.tri(result, diag = FALSE)] <- 0
-        window = 0L
-        weights = 1
-        # make sure that zeros are sparse
-        # result[result==0] <- 0
+        window <- max(lengths(x))
+    }        
+        # x <- tf(dfm(x, toLower = FALSE, verbose = FALSE), "boolean")
+        # # get co-occurrence counts
+        # result <- t(x) %*% x
+        # # remove 1 from the diagonal so that target features are not counted as their own context
+        # # Matrix::diag(result) <- ifelse(Matrix::diag(result) > 0, Matrix::diag(result) - 1, 0)
+        # # return triangular result only if tri == TRUE
+        # if (tri) 
+        #     result[lower.tri(result, diag = FALSE)] <- 0
+        # window = 0L
+        # weights = 1
+        # # make sure that zeros are sparse
+        # # result[result==0] <- 0
 
-    } else if (context == "window") {
+#    } else if (context == "window") {
         
-        x <- ngrams(unlist(x), n = window, skip = 0:(window - 1)) #, ...)
-        x <- strsplit(x, "_")
-        # create a data.table of tokens
-        x <- data.table(do.call(rbind, x))
-        setnames(x, 1:2, c("context", "feature"))
-        x <- x[, paste(feature, collapse = " "), by = context]
-        # make into a character vector
-        contexts <- x[, V1]
-        names(contexts) <- x[, context]
-        rm(x)
-        result <- dfm(contexts, toLower = FALSE, verbose = FALSE)
+        # x <- ngrams(unlist(x), n = window, skip = 0:(window - 1)) #, ...)
+        # x <- strsplit(x, "_")
+        # # create a data.table of tokens
+        # x <- data.table(do.call(rbind, x))
+        # setnames(x, 1:2, c("context", "feature"))
+        # x <- x[, paste(feature, collapse = " "), by = context]
+        # # make into a character vector
+        # contexts <- x[, V1]
+        # names(contexts) <- x[, context]
+        # rm(x)
+        # result <- dfm(contexts, toLower = FALSE, verbose = FALSE)
         
-    }
+        types <- unique(unlist(x, use.names = FALSE))
+        n <- sum(lengths(x)) * window * 2
+        y <- fcm_cpp(x, types, window, n)
+        
+        # if (verbose) message("Making sparseMatrix\n")
+        result <- Matrix::sparseMatrix(i = y$target, 
+                                       j = y$collocate, 
+                                       x = 1L,
+                                       dimnames = list(contexts = types, features = types))
+ #   }
 
-    result <- new("fcm", as(result, "dgCMatrix"), context = context, window = window, weights = weights)
-    names(result@Dimnames) <- c("contextFeatures", "targetFeatures")
+    # order the features alphabetically
+    result <- result[order(rownames(result)), order(colnames(result))]
+    result <- new("cfm", as(result, "dgCMatrix"), context = context, window = window, weights = weights)
+    names(result@Dimnames) <- c("contexts", "features")
     result
 }     
 
 
 
-#' @rdname fcm
+#' @rdname cfm
 #' @export
-fcm.corpus <- function(x, ...) {
-    x <- tokenize(x, ...)
-    fcm(x)
+cfm.corpus <- function(x, ...) {
+    cfm(texts(x), ...)
 }
 
-#' @rdname fcm
+#' @rdname cfm
 #' @export
-fcm.character <- function(x, ...) {
-    x <- tokenize(x, ...)
-    fcm(x)
+cfm.character <- function(x, ...) {
+    cfm(tokenize(x), ...)
 }
 
 
 #' @rdname print.dfm
 #' @export
-setMethod("print", signature(x = "fcm"), 
+setMethod("print", signature(x = "cfm"), 
           function(x, show.values = FALSE, show.settings = FALSE, show.summary = TRUE, nfeature = 20L, ...) {
               ndoc <- nfeature
               if (show.summary) {
                   cat("Context-feature matrix of: ",
-                      format(ndoc(x), , big.mark=","), " context feature",
-                      ifelse(ndoc(x)>1 | ndoc(x)==0, "s, ", ", "),
-                      format(nfeature(x), big.mark=","), " target feature",
-                      ifelse(nfeature(x)>1 | nfeature(x)==0, "s", ""),
-                      ifelse(is.resampled(x), paste(", ", nresample(x), " resamples", sep=""), ""),
-                      ".\n", sep="")
+                      format(ndoc(x), , big.mark = ","), " context",
+                      ifelse(ndoc(x) > 1 | ndoc(x) == 0, "s, ", ", "),
+                      format(nfeature(x), big.mark = ","), " feature",
+                      ifelse(nfeature(x) > 1 | nfeature(x) == 0, "s", ""),
+                      ifelse(is.resampled(x), paste(", ", nresample(x), " resamples", sep = ""), ""),
+                      ".\n", sep = "")
               }
               if (show.settings) {
                   cat("Settings: TO BE IMPLEMENTED.")
               }
-              if (show.values | (nrow(x)<=ndoc & ncol(x)<=nfeature)) {
+              if (show.values | (nrow(x) <= ndoc & ncol(x) <= nfeature)) {
                   Matrix::printSpMatrix2(x[1:min(ndoc, ndoc(x)), 1:min(nfeature, nfeature(x))], 
-                                         col.names=TRUE, zero.print=0, ...)
+                                         col.names = TRUE, zero.print = 0, ...)
               }
           })
 
 #' @rdname print.dfm
-setMethod("show", signature(object = "fcm"), function(object) print(object))
+setMethod("show", signature(object = "cfm"), function(object) print(object))
 
 
-#' C++ version of fcm
-#' @rdname fcm
-#' @examples
-#' txt <- c("The quick brown fox jumped over the lazy dog.",
-#'          "The dog jumped and ate the fox.")
-#' toks <- tokenize(toLower(txt), removePunct = TRUE)
-#' fcm2.tokenizedTexts(toks, context = "window", window = 3)
-#' fcm2.tokenizedTexts(list(letters), context = "window", window = 3)
-#' @export
-fcm2.tokenizedTexts <- function(x, context = c("document", "window"), window = 5L, verbose=FALSE){
-  
-  if(context == 'document'){
-      window <- max(lengths(x))
-  }
-  types <- unique(unlist(x, use.names=FALSE))
-  n <- sum(lengths(x)) * window * 2
-  y <- fcm_cpp(x, types, window, n)
-  
-  if (verbose) message("Making sparseMatrix\n")
-  mx <- Matrix::sparseMatrix(i = y$target, 
-                             j = y$collocate, 
-                             x = 1L,
-                             dimnames = list(context = types, features = types))
-  
-  return(mx)
-}
-
+# # C++ wrapper for cfm()
+# # Author: Kohei Watanabe
+# cfmCpp_tokenizedTexts <- function(x, context = c("document", "window"), window = 5L, verbose = FALSE) {
+#     context <- match.arg(context)
+#     if (context == 'document') {
+#         window <- max(lengths(x))
+#     }
+#     types <- unique(unlist(x, use.names=FALSE))
+#     n <- sum(lengths(x)) * window * 2
+#     y <- fcm_cpp(x, types, window, n)
+#     
+#     if (verbose) message("Making sparseMatrix\n")
+#     mx <- Matrix::sparseMatrix(i = y$target, 
+#                                j = y$collocate, 
+#                                x = 1L,
+#                                dimnames = list(context = types, features = types))
+#     
+#     return(mx)
+# }
+# 
 
  
 
